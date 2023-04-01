0bd9f44a2adae8033a44933b10d2a3612c1456e9
MarkXtenda <mark.i2p.1998@gmail.com>
2023-04-01 14:02:27 +1100

    done

--- src/reducer.js
+++ src/reducer.js
@@ -10,4 +10,17 @@ function changeState(state, action) {
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  return state;
+}
+
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+dispatch({ type: "counter/increment" });
+dispatch({ type: "counter/increment" });
+
+// call the render function
+render();
